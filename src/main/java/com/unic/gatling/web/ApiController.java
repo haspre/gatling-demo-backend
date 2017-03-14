25cbce243cede9f959010bb4117cc77d686d54f2
Rainer Steinegger <rainer.steinegger@citrix.com>
2017-03-14 09:19:17 +0100

    Added initial rest controller

---
+++ src/main/java/com/unic/gatling/web/ApiController.java
@@ -0,0 +1,34 @@
+package com.unic.gatling.web;
+
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static java.lang.Math.abs;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@RestController
+public class ApiController {
+
+    private final Map<Long, Resource> resources = new ConcurrentHashMap<>();
+    private final Random random = new Random();
+
+    @RequestMapping(path = "/resources", method = POST)
+    public Resource create() {
+        final Resource resource = new Resource(abs(random.nextLong()));
+        resources.put(resource.id, resource);
+        return resource;
+    }
+
+
+    @RequestMapping(path = "/resources/{id}", method = GET)
+    public Resource get(@PathVariable("id") Long id) {
+        return resources.get(id);
+    }
+}
